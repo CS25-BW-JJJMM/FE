23369ab5236e608e611d980f39d0651424e95641
Jonathan Dorety <kyullog@gmail.com>
2020-02-04 13:42:14 -0800

    Create game component. Movement controls function

--- front-end/src/Components/Game/Game.js
+++ front-end/src/Components/Game/Game.js
@@ -1,13 +1,60 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
+
+import axiosHeader from "../../utils/axios";
 import ControlPad from "./Controls/ControlPad";
 import DescriptionBox from "./Display/DescriptionBox";
+import MapDisplay from "./Display/MapDisplay";
+
+const myAxios = axiosHeader.axiosHeaders();
 
 export default function Game(props) {
+  const [currentRoom, setRoom] = useState({});
+  const [name, setName] = useState("");
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    myAxios
+      .get("/api/adv/init/")
+      .then(res => {
+        console.log(res);
+        const { data } = res;
+        setRoom({
+          title: data.title,
+          description: data.description,
+          players: data.players
+        });
+        setName(data.name);
+        setError(data.error_msg);
+      })
+      .catch(err => console.log(err));
+  }, []);
+
+  const move = async dir => {
+    try {
+      const direction = { direction: `${dir}` };
+      const movement = await myAxios.post("/api/adv/move/", direction);
+      console.log(movement);
+      const { data } = movement;
+      setRoom({
+        title: data.title,
+        description: data.description,
+        players: data.players
+      });
+      if (data.error_msg) {
+        setError(data.error_msg);
+      }
+    } catch (err) {
+      console.log(err);
+      setError(err);
+    }
+  };
+
   return (
     <div>
-      <ControlPad />
-      <DescriptionBox description="Whoop whoop where we at!" />
+      <ControlPad movement={move} />
+      <DescriptionBox description={currentRoom.description} error={error} />
+      <MapDisplay />
     </div>
   );
 }
