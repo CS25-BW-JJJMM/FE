a59718c543d8ec5bf949872b727d73e7274afd45
Jonathan Dorety <kyullog@gmail.com>
2020-02-03 22:47:52 -0800

    Create panel to hold directional buttons

--- front-end/src/Components/Game/Controls/ControlPad.js
+++ front-end/src/Components/Game/Controls/ControlPad.js
@@ -2,34 +2,44 @@ import React from "react";
 import styled from "styled-components";
 import DirButton from "./DirButton";
 
-import "../../../../node_modules/nes.css/css/nes.min.css";
-
+// holds the directional buttons, points them via props
 const ControlPad = props => {
   return (
-    <ControlsWrapper className="nes-container is-rounded">
-      <DirButton direction="w" />
-      <MiddleButtons>
-        <DirButton direction="s" />
-        <DirButton direction="n" />
-      </MiddleButtons>
-      <DirButton direction="e" />
-    </ControlsWrapper>
+    <Panel>
+      <ControlsWrapper>
+        <DirButton direction="w" />
+        <MiddleButtons>
+          <DirButton direction="n" />
+          <DirButton direction="s" />
+        </MiddleButtons>
+        <DirButton direction="e" />
+      </ControlsWrapper>
+    </Panel>
   );
 };
 
-const ControlsWrapper = styled.div`
-  max-width: 175px;
+const Panel = styled.div`
+  max-width: 200px;
   width: 100%;
+`;
+
+const ControlsWrapper = styled.div`
   display: flex;
   align-items: center;
   justify-content: center;
-  padding: 0;
+  background-color: #35654d;
+  border-style: solid;
+  border-color: #755b69;
+  border-width: 3px;
+  border-radius: 60px;
 `;
 
 const MiddleButtons = styled.div`
   display: flex;
-  flex-direction: column-reverse;
+  flex-direction: column;
   justify-content: space-between;
+  height: 125px;
+  margin: 10px 2%;
 `;
 
 export default ControlPad;
